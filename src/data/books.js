3106e498f17db27566d8597e9155f933b4906a68
Karman Karman <karman131313@gmail.com>
2023-07-13 01:17:04 +0200

    view-transition-name connect images with dt images

---
+++ src/data/books.js
@@ -0,0 +1,55 @@
+export const books = [
+	{
+		id: 'clean-code',
+		title: 'Clean Code: Manual de desarrollo ágil de software',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg',
+		author: 'Robert C. Martin',
+		opinion:
+			'Clean Code: Manual de desarrollo ágil de software es una lectura imprescindible para cualquier desarrollador. El autor, Robert C. Martin, brinda valiosos consejos y técnicas para escribir un código limpio, legible y mantenible. A través de ejemplos prácticos y principios sólidos, el libro promueve una mentalidad de calidad en el desarrollo de software. Con sus recomendaciones, Martin muestra cómo mejorar la claridad, la eficiencia y la colaboración en los proyectos. Clean Code es una guía inspiradora que ayuda a elevar el estándar de la programación y a construir software robusto y de calidad.',
+		rate: 8.7
+	},
+	{
+		id: 'the-clean-coder',
+		title: 'The Clean Coder: A Code of Conduct for Professional Programmers',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg',
+		author: 'Robert C. Martin',
+		opinion:
+			'The Clean Coder: A Code of Conduct for Professional Programmers es una lectura esencial para todo desarrollador que aspira a ser un profesional destacado. Escrito por Robert C. Martin, este libro ofrece valiosas lecciones sobre ética, responsabilidad y excelencia en la programación. Con anécdotas y consejos prácticos, Martin aborda los desafíos comunes en el ámbito de la programación y propone pautas para mantener altos estándares de calidad en el trabajo. The Clean Coder inspira a los programadores a ser disciplinados, comunicativos y comprometidos con su oficio, fomentando una cultura de respeto y colaboración en la industria del software. Una lectura obligada para aquellos que buscan ser profesionales excepcionales en su campo.'
+	},
+	{
+		id: 'pracmatic-programmer',
+		title: 'The Pragmatic Programmer: From Journeyman to Master',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg',
+		author: 'Andrew Hunt y David Thomas',
+		opinion:
+			'The Pragmatic Programmer: From Journeyman to Master is a must-read for aspiring software developers looking to level up their skills. Written by Andrew Hunt and David Thomas, this book offers a wealth of practical advice and timeless wisdom for becoming a highly effective programmer. It covers a wide range of topics, including code organization, debugging, testing, and teamwork, providing valuable insights that can be applied in any programming language or environment. The Pragmatic Programmer encourages a mindset of continuous learning, innovation, and professionalism, guiding readers on their journey to mastery. With its pragmatic approach and actionable tips, this book serves as a trusted companion for programmers at any stage of their careers.',
+		rate: 8.3
+	},
+	{
+		id: 'js-good-parts',
+		title: 'JavaScript: The Good Parts',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1328834793i/2998152.jpg',
+		author: 'Douglas Crockford',
+		opinion:
+			'JavaScript: The Good Parts is a concise and insightful guide that focuses on the best features and practices of the JavaScript programming language. Authored by Douglas Crockford, this book highlights the language`s strengths while also shedding light on its pitfalls to help developers write cleaner and more reliable code. Crockford distills the essence of JavaScript, covering topics such as functions, objects, arrays, and inheritance, and provides practical advice for avoiding common mistakes. With its emphasis on simplicity and elegance, JavaScript: The Good Parts is an invaluable resource for both beginner and experienced JavaScript developers looking to write high-quality code that is efficient, maintainable, and robust.',
+		rate: 7.5
+	},
+	{
+		id: 'desing-data-intensive',
+		title: 'Designing Data-Intensive Applications',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1415816873i/23463279.jpg',
+		author: 'Martin Kleppmann',
+		opinion:
+			'Designing Data-Intensive Applications is an essential read for anyone involved in designing or building data-intensive systems. Authored by Martin Kleppmann, this book offers a comprehensive exploration of the principles, techniques, and trade-offs involved in building scalable and reliable data systems. Kleppmann covers a wide range of topics, including data models, storage and retrieval, data processing, and distributed systems. With a blend of theory and real-world examples, the book provides deep insights into the challenges of handling large-scale data and offers practical guidance for designing systems that are both performant and resilient. Designing Data-Intensive Applications is an invaluable resource for engineers and architects seeking to create robust and scalable data solutions.',
+		rate: 9.2
+	},
+	{
+		id: 'eloquent-javascript',
+		title: 'Eloquent JavaScript: A Modern Introduction to Programming',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1308260856i/8910666.jpg',
+		author: 'Marijn Haverbeke',
+		opinion:
+			'Eloquent JavaScript: A Modern Introduction to Programming is a fantastic resource for beginners looking to learn JavaScript and programming concepts. Written by Marijn Haverbeke, this book provides a comprehensive and approachable introduction to the language. It covers fundamental programming principles, such as variables, functions, control flow, and data structures, while also delving into more advanced topics like object-oriented programming and asynchronous programming. With its clear explanations and hands-on exercises, Eloquent JavaScript enables readers to develop a solid foundation in programming. The book also explores real-world applications and practical examples, making it an engaging and practical learning tool for aspiring developers.',
+		rate: 9.8
+	}
+]
